3b595e4ce65af6d7427964acd42be2f36ae77174
Steven <steven.shoemaker@utdallas.edu>
2022-12-11 00:47:55 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,60 +2,60 @@ import streamlit as st
 import pandas as pd
 
 COLOR_RED = "#FF4B4B"
-    COLOR_BLUE = "#1C83E1"
-    COLOR_CYAN = "#00C0F2"
+COLOR_BLUE = "#1C83E1"
+COLOR_CYAN = "#00C0F2"
 
-    def display_callout(title, color, icon, second_text):
-        st.markdown(
-            div(
-                style=styles(
-                    background_color=color,
-                    padding=rem(1),
-                    display="flex",
-                    flex_direction="row",
-                    border_radius=rem(0.5),
-                    margin=(0, 0, rem(0.5), 0),
-                )
-            )(
-                div(style=styles(font_size=rem(2), line_height=1))(icon),
-                div(style=styles(padding=(rem(0.5), 0, rem(0.5), rem(1))))(title),
+def display_callout(title, color, icon, second_text):
+    st.markdown(
+        div(
+            style=styles(
+                background_color=color,
+                padding=rem(1),
+                display="flex",
+                flex_direction="row",
+                border_radius=rem(0.5),
+                margin=(0, 0, rem(0.5), 0),
+            )
+        )(
+            div(style=styles(font_size=rem(2), line_height=1))(icon),
+            div(style=styles(padding=(rem(0.5), 0, rem(0.5), rem(1))))(title),
+        ),
+        unsafe_allow_html=True,
+    )
+
+def display_small_text(text):
+    st.markdown(
+        div(
+            style=styles(
+                font_size=rem(0.8),
+                margin=(0, 0, rem(1), 0),
+            )
+        )(text),
+        unsafe_allow_html=True,
+    )
+
+def display_dial(title, value, color):
+    st.markdown(
+        div(
+            style=styles(
+                text_align="center",
+                color=color,
+                padding=(rem(0.8), 0, rem(3), 0),
+            )
+        )(
+            h2(style=styles(font_size=rem(0.8), font_weight=600, padding=0))(title),
+            big(style=styles(font_size=rem(3), font_weight=800, line_height=1))(
+                value
             ),
-            unsafe_allow_html=True,
-        )
+        ),
+        unsafe_allow_html=True,
+    )
 
-    def display_small_text(text):
-        st.markdown(
-            div(
-                style=styles(
-                    font_size=rem(0.8),
-                    margin=(0, 0, rem(1), 0),
-                )
-            )(text),
-            unsafe_allow_html=True,
-        )
-
-    def display_dial(title, value, color):
-        st.markdown(
-            div(
-                style=styles(
-                    text_align="center",
-                    color=color,
-                    padding=(rem(0.8), 0, rem(3), 0),
-                )
-            )(
-                h2(style=styles(font_size=rem(0.8), font_weight=600, padding=0))(title),
-                big(style=styles(font_size=rem(3), font_weight=800, line_height=1))(
-                    value
-                ),
-            ),
-            unsafe_allow_html=True,
-        )
-
-    def display_dict(dict):
-        for k, v in dict.items():
-            a, b = st.columns([1, 4])
-            a.write(f"**{k}:**")
-            b.write(v)
+def display_dict(dict):
+    for k, v in dict.items():
+        a, b = st.columns([1, 4])
+        a.write(f"**{k}:**")
+        b.write(v)
 
 
 
